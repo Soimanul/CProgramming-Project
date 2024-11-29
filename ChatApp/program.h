18181d78a79dc7f6a50385c225efe12431e5971f
hibba kamas <hibbakamas@gmail.com>
2024-11-29 17:40:30 +0100

    individual message works, chatroom message works, broadcast works

---
+++ ChatApp/program.h
@@ -0,0 +1,67 @@
+#ifndef PROGRAM_H
+#define PROGRAM_H
+
+#include <stdio.h>      // For input/output functions like printf, fgets, and perror
+#include <stdlib.h>     // For dynamic memory allocation and general utilities
+#include <string.h>     // For string manipulation functions like strcmp, strlen, strcspn
+#include <pthread.h>    // For thread handling with pthread_create
+
+#ifdef _WIN32
+// Windows-specific libraries
+#include <winsock2.h>   // For socket programming on Windows
+#include <ws2tcpip.h>   // For InetPton and related network functions
+#pragma comment(lib, "Ws2_32.lib")  // Link with Ws2_32.lib for Windows socket functionality
+typedef int socklen_t;  // Define socklen_t for Windows compatibility
+#else
+// Non-Windows-specific libraries
+#include <sys/socket.h> // For socket creation and connection
+#include <netinet/in.h> // For sockaddr_in and related network structures
+#include <arpa/inet.h>  // For inet_pton to convert IP addresses
+#include <unistd.h>     // For close function to close sockets
+#endif
+
+// Constants
+#define PORT 8080           // Port number for the server
+#define BUFFER_SIZE 1024    // Size of the buffer for messages
+#define MAX_CLIENTS 100     // Maximum number of clients the server can handle
+#define MAX_ROOMS 10        // Maximum number of chat rooms
+#define MAX_ROOM_NAME 50    // Maximum length of a room name
+
+
+// Declare global structures
+typedef struct {
+    int sock;
+    char username[50];
+    char room[MAX_ROOM_NAME];
+} Client;
+
+typedef struct {
+    char name[MAX_ROOM_NAME];
+    Client *clients[MAX_CLIENTS];
+    int client_count;
+    char creator_username[50];  // Add this line to store the creator's username
+} ChatRoom;
+
+
+void *receive_messages(void *sock); 
+
+// Global Variables (extern declaration)
+extern Client clients[MAX_CLIENTS];           // Array of clients
+extern ChatRoom chat_rooms[MAX_ROOMS];        // Array of chat rooms
+extern int client_count;                      // Number of connected clients
+extern int room_count;                        // Number of chat rooms
+extern pthread_mutex_t clients_mutex;         // Mutex for clients array
+extern pthread_mutex_t rooms_mutex;           // Mutex for chat rooms array
+
+// Function Prototypes
+int init_socket();  // Initialize server socket and return the socket descriptor
+void broadcast_message(const char *message, int exclude_sock);  // Broadcast message to clients in the same room (excluding a specific socket)
+void list_online_users(int sock);  // List all online users for a client
+void list_chat_rooms(int sock);    // List all available chat rooms for a client
+void join_chat_room(const char *room_name, Client *client);   // Join a chat room
+void create_chat_room(const char *room_name, Client *creator, char *users);  // Create a new chat room
+void room_message(const char *room_name, const char *message, const char *sender);  // Send a message to all clients in a room
+void handle_client(int client_sock);  // Handle client communication
+void *client_handler(void *client_sock_ptr);  // Threaded function to handle client communication
+
+#endif
--- ChatApp/server.c
+++ ChatApp/server.c
@@ -1,57 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <pthread.h>
+#include "program.h"
 
-#ifdef _WIN32
-#include <winsock2.h>
-#pragma comment(lib, "Ws2_32.lib") 
-typedef int socklen_t;  
-#else
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#endif
 
-#define PORT 8080
-#define BUFFER_SIZE 1024
-#define MAX_CLIENTS 100
-#define MAX_ROOMS 10
-#define MAX_ROOM_NAME 50
+// Global Variables
+Client clients[MAX_CLIENTS];           // Initialize the array of clients
+ChatRoom chat_rooms[MAX_ROOMS];        // Initialize the array of chat rooms
+int client_count = 0;                  // Initialize the client count to 0
+int room_count = 0;                    // Initialize the room count to 0
+pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;  // Initialize the mutex for clients
+pthread_mutex_t rooms_mutex = PTHREAD_MUTEX_INITIALIZER;    // Initialize the mutex for rooms
 
-typedef struct {
-    int sock;
-    char username[50];
-    char room[MAX_ROOM_NAME];
-} Client;
 
-typedef struct {
-    char name[MAX_ROOM_NAME];
-    Client *clients[MAX_CLIENTS];
-    int client_count;
-} ChatRoom;
 
-Client clients[MAX_CLIENTS];
-ChatRoom chat_rooms[MAX_ROOMS];
-int client_count = 0;
-int room_count = 0;
-pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t rooms_mutex = PTHREAD_MUTEX_INITIALIZER;
-
-int init_socket();
-void broadcast_message(const char *message, int exclude_sock);
-void list_online_users(int sock);
-void list_chat_rooms(int sock);
-void join_chat_room(const char *room_name, Client *client);
-void create_chat_room(const char *room_name, Client *creator, char *users);
-void room_message(const char *room_name, const char *message, const char *sender);
-void handle_client(int client_sock);
-void *client_handler(void *client_sock_ptr);
 
 int init_socket() {
     int sock;
     struct sockaddr_in server_addr;
+
 
     #ifdef _WIN32
     WSADATA wsaData;
@@ -61,25 +25,30 @@ int init_socket() {
     }
     #endif
 
+
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1) {
         perror("Socket creation failed");
         return -1;
     }
 
+
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
+
 
     if (bind(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         perror("Bind failed");
         return -1;
     }
 
+
     if (listen(sock, 3) < 0) {
         perror("Listen failed");
         return -1;
     }
+
 
     struct sockaddr_in sin;
     socklen_t len = sizeof(sin);
@@ -89,8 +58,10 @@ int init_socket() {
         printf("Server is running on IP: %s, Port: %d\n", inet_ntoa(sin.sin_addr), ntohs(sin.sin_port));
     }
 
+
     return sock;
 }
+
 
 void broadcast_message(const char *message, int exclude_sock) {
     pthread_mutex_lock(&clients_mutex);
@@ -103,6 +74,7 @@ void broadcast_message(const char *message, int exclude_sock) {
     }
     pthread_mutex_unlock(&clients_mutex);
 }
+
 
 void list_online_users(int sock) {
     pthread_mutex_lock(&clients_mutex);
@@ -117,6 +89,7 @@ void list_online_users(int sock) {
     pthread_mutex_unlock(&clients_mutex);
 }
 
+
 void list_chat_rooms(int sock) {
     pthread_mutex_lock(&rooms_mutex);
     char message[BUFFER_SIZE] = "Chat rooms:\n";
@@ -130,13 +103,14 @@ void list_chat_rooms(int sock) {
     pthread_mutex_unlock(&rooms_mutex);
 }
 
+
 void join_chat_room(const char *room_name, Client *client) {
     pthread_mutex_lock(&rooms_mutex);
     for (int i = 0; i < room_count; i++) {
         if (strcmp(chat_rooms[i].name, room_name) == 0) {
             chat_rooms[i].clients[chat_rooms[i].client_count++] = client;
             strcpy(client->room, room_name);
-            printf("Client %s joined chat room %s\n", client->username, room_name); 
+            printf("Client %s joined chat room %s\n", client->username, room_name);
             pthread_mutex_unlock(&rooms_mutex);
             return;
         }
@@ -146,43 +120,71 @@ void join_chat_room(const char *room_name, Client *client) {
     chat_rooms[room_count].client_count = 1;
     strcpy(client->room, room_name);
     room_count++;
-    printf("Chat room %s created and client %s joined\n", room_name, client->username); 
+    printf("Chat room %s created and client %s joined\n", room_name, client->username);
     pthread_mutex_unlock(&rooms_mutex);
 }
 
+
 void create_chat_room(const char *room_name, Client *creator, char *users) {
     pthread_mutex_lock(&rooms_mutex);
+   
     strcpy(chat_rooms[room_count].name, room_name);
+    strcpy(chat_rooms[room_count].creator_username, creator->username);  // Store the creator's username
     chat_rooms[room_count].clients[0] = creator;
     chat_rooms[room_count].client_count = 1;
     strcpy(creator->room, room_name);
+   
+    printf("Creator room set to: %s\n", creator->room);
+
 
     char *username = strtok(users, " ");
     while (username != NULL) {
         for (int i = 0; i < client_count; i++) {
             if (strcmp(clients[i].username, username) == 0) {
+                printf("Adding user to room: %s\n", username);
                 chat_rooms[room_count].clients[chat_rooms[room_count].client_count++] = &clients[i];
                 strcpy(clients[i].room, room_name);
+                printf("User %s room set to: %s\n", username, clients[i].room);
                 break;
             }
         }
         username = strtok(NULL, " ");
     }
 
+
     room_count++;
-    printf("Chat room %s created", room_name); 
+    printf("Room created. Room count: %d, Room clients: %d\n", room_count, chat_rooms[room_count-1].client_count);
     pthread_mutex_unlock(&rooms_mutex);
 }
-
 void room_message(const char *room_name, const char *message, const char *sender) {
     pthread_mutex_lock(&rooms_mutex);
     char full_message[BUFFER_SIZE];
     snprintf(full_message, BUFFER_SIZE, "%s/%s: %s", room_name, sender, message);
 
+
+    printf("Attempting to send room message\n");
+    printf("Room: %s, Sender: %s\n", room_name, sender);
+    printf("Total rooms: %d\n", room_count);
+
+
     for (int i = 0; i < room_count; i++) {
+        printf("Checking room %d: %s\n", i, chat_rooms[i].name);
         if (strcmp(chat_rooms[i].name, room_name) == 0) {
+            printf("Found matching room. Clients in room: %d\n", chat_rooms[i].client_count);
+           
             for (int j = 0; j < chat_rooms[i].client_count; j++) {
-                if (strcmp(chat_rooms[i].clients[j]->username, sender) != 0) {
+                printf("Client %d - Username: %s, Assigned Room: %s\n",
+                       j,
+                       chat_rooms[i].clients[j]->username,
+                       chat_rooms[i].clients[j]->room);
+            }
+
+
+            for (int j = 0; j < chat_rooms[i].client_count; j++) {
+                if (strcmp(chat_rooms[i].clients[j]->room, room_name) == 0 &&
+                    strcmp(chat_rooms[i].clients[j]->username, sender) != 0) {
+                    printf("Sending message to: %s\n", chat_rooms[i].clients[j]->username);
+                   
                     if (send(chat_rooms[i].clients[j]->sock, full_message, strlen(full_message), 0) < 0) {
                         perror("Send failed");
                     }
@@ -194,8 +196,9 @@ void room_message(const char *room_name, const char *message, const char *sender
     pthread_mutex_unlock(&rooms_mutex);
 }
 
+
 void display_menu(int client_sock) {
-    const char *menu = 
+    const char *menu =
         "Menu:\n"
         "1. Private message\n"
         "2. Create a room\n"
@@ -208,10 +211,12 @@ void display_menu(int client_sock) {
     send(client_sock, menu, strlen(menu), 0);
 }
 
+
 void handle_client(int client_sock) {
     char buffer[BUFFER_SIZE];
     int read_size;
     char username[50];
+
 
     if ((read_size = recv(client_sock, username, 50, 0)) > 0) {
         username[read_size] = '\0';
@@ -224,13 +229,17 @@ void handle_client(int client_sock) {
         printf("%s joined the chat\n", username);
     }
 
+
     display_menu(client_sock);
+
 
     while ((read_size = recv(client_sock, buffer, BUFFER_SIZE, 0)) > 0) {
         buffer[read_size] = '\0';
         printf("Received message from %s: %s\n", username, buffer);
 
+
         buffer[strcspn(buffer, "\r\n")] = 0;
+
 
         int choice = atoi(buffer);
         switch (choice) {
@@ -242,12 +251,14 @@ void handle_client(int client_sock) {
                 strcpy(target_username, buffer);
                 target_username[strcspn(target_username, "\r\n")] = 0;
 
+
                 send(client_sock, "Enter message: ", 15, 0);
                 read_size = recv(client_sock, buffer, BUFFER_SIZE, 0);
                 buffer[read_size] = '\0';
                 char message[BUFFER_SIZE];
                 strcpy(message, buffer);
                 message[strcspn(message, "\r\n")] = 0;
+
 
                 int found = 0;
                 pthread_mutex_lock(&clients_mutex);
@@ -268,6 +279,7 @@ void handle_client(int client_sock) {
                 }
                 pthread_mutex_unlock(&clients_mutex);
 
+
                 if (!found) {
                     if (send(client_sock, "User not found.\n", 17, 0) < 0) {
                         perror("Send failed");
@@ -276,6 +288,7 @@ void handle_client(int client_sock) {
                 }
                 break;
             }
+            // In your handle_client function, modify the room creation case
             case 2: {
                 send(client_sock, "Enter room name: ", 17, 0);
                 read_size = recv(client_sock, buffer, BUFFER_SIZE, 0);
@@ -284,6 +297,7 @@ void handle_client(int client_sock) {
                 strcpy(room_name, buffer);
                 room_name[strcspn(room_name, "\r\n")] = 0;
 
+
                 send(client_sock, "Enter usernames (space-separated): ", 35, 0);
                 read_size = recv(client_sock, buffer, BUFFER_SIZE, 0);
                 buffer[read_size] = '\0';
@@ -291,7 +305,25 @@ void handle_client(int client_sock) {
                 strcpy(users, buffer);
                 users[strcspn(users, "\r\n")] = 0;
 
-                create_chat_room(room_name, &clients[client_count - 1], users);
+
+                // Find the client who is creating the room by their socket
+                Client *room_creator = NULL;
+                pthread_mutex_lock(&clients_mutex);
+                for (int i = 0; i < client_count; i++) {
+                    if (clients[i].sock == client_sock) {
+                        room_creator = &clients[i];
+                        break;
+                    }
+                }
+                pthread_mutex_unlock(&clients_mutex);
+
+
+                if (room_creator) {
+                    create_chat_room(room_name, room_creator, users);
+                } else {
+                    // Handle error: couldn't find room creator
+                    send(client_sock, "Error creating room\n", 20, 0);
+                }
                 break;
             }
             case 3: {
@@ -302,12 +334,14 @@ void handle_client(int client_sock) {
                 strcpy(room_name, buffer);
                 room_name[strcspn(room_name, "\r\n")] = 0;
 
+
                 send(client_sock, "Enter message: ", 15, 0);
                 read_size = recv(client_sock, buffer, BUFFER_SIZE, 0);
                 buffer[read_size] = '\0';
                 char message[BUFFER_SIZE];
                 strcpy(message, buffer);
                 message[strcspn(message, "\r\n")] = 0;
+
 
                 room_message(room_name, message, username);
                 break;
@@ -326,6 +360,7 @@ void handle_client(int client_sock) {
                 strcpy(room_name, buffer);
                 room_name[strcspn(room_name, "\r\n")] = 0;
 
+
                 join_chat_room(room_name, &clients[client_count - 1]);
                 break;
             }
@@ -337,6 +372,7 @@ void handle_client(int client_sock) {
                 strcpy(message, buffer);
                 message[strcspn(message, "\r\n")] = 0;
 
+
                 broadcast_message(message, client_sock);
                 break;
             }
@@ -345,14 +381,17 @@ void handle_client(int client_sock) {
                 break;
         }
 
+
         display_menu(client_sock);
     }
+
 
     if (read_size == 0) {
         printf("Client disconnected\n");
     } else if (read_size == -1) {
         perror("Recv failed");
     }
+
 
     #ifdef _WIN32
     closesocket(client_sock);
@@ -361,39 +400,47 @@ void handle_client(int client_sock) {
     #endif
 }
 
+
 void *client_handler(void *client_sock_ptr) {
     int client_sock = *(int *)client_sock_ptr;
-    free(client_sock_ptr); 
+    free(client_sock_ptr);
     handle_client(client_sock);
     return NULL;
 }
+
 
 int main() {
     int server_sock, client_sock;
     struct sockaddr_in client_addr;
     socklen_t client_addr_len = sizeof(client_addr);
 
+
     server_sock = init_socket();
     if (server_sock == -1) {
         return 1;
     }
 
+
     printf("Server listening on port %d\n", PORT);
+
 
     while ((client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_addr_len)) >= 0) {
         printf("Client connected\n");
         pthread_t tid;
-        int *client_sock_ptr = malloc(sizeof(int)); 
+        int *client_sock_ptr = malloc(sizeof(int));
         *client_sock_ptr = client_sock;
+
 
         if (pthread_create(&tid, NULL, client_handler, (void *)client_sock_ptr) != 0) {
             perror("Thread creation failed");
         }
     }
 
+
     if (client_sock < 0) {
         perror("Accept failed");
     }
+
 
     #ifdef _WIN32
     closesocket(server_sock);
@@ -401,6 +448,7 @@ int main() {
     #else
     close(server_sock);
     #endif
+
 
     return 0;
 }
